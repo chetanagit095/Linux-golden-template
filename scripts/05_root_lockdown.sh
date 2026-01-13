49acc712a7e95398074cc115d9397719f3959bb9
Chetana Sonawane <chetanasonawane095@gmail.com>
2026-01-13 18:59:50 +0530

    05_root_lockdown.sh

--- scripts/05_root_lockdown.sh
+++ scripts/05_root_lockdown.sh
@@ -1,1 +1,4 @@
-
+-  After logging in as subadmin:
+  - The root account was disabled and locked for security purposes using:
+       passwd -l root
+   - SSH root login was also restricted again, if applicable.

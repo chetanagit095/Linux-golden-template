95880e7e525b5657523b08aadb940640fd232081
Chetana Sonawane <chetanasonawane095@gmail.com>
2026-01-13 18:53:36 +0530

    04_user_privilege_setup.sh

--- scripts/04_user_privilege_setup.sh
+++ scripts/04_user_privilege_setup.sh
@@ -1,1 +1,5 @@
-
+- Two administrative users were created:
+  mainadmin – Emergency access only
+  subadmin – Daily administrative tasks
+- Both users were added to the sudo group to grant administrative privileges. 
+- A system reboot followed to ensure proper user configuration. 

408ea2e1548cda6453246173e93ef5635c86d007
Chetana Sonawane <chetanasonawane095@gmail.com>
2026-01-13 18:47:36 +0530

    02_ssh_root_initial_access.sh

--- scripts/02_ssh_root_initial_access.sh
+++ scripts/02_ssh_root_initial_access.sh
@@ -1,1 +1,5 @@
-
+- The system initially had a default user: ubuntu. 
+- From this user session
+  The root password was set using the passwd command. 
+  In the SSH configuration file /etc/ssh/sshd_config, the parameter PermitRootLogin was set to yes to allow SSH login as root. 
+- The system was then rebooted.
